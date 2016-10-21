8dd7366647cfbe20407c05b9446c8b15b6c909fc
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-20 18:12:45 -0600

    basic server

--- server/server.js
+++ server/server.js
@@ -7,3 +7,6 @@ app.set('port', 3000);
 app.listen(app.get('port'), function() {
   console.log('Server listening on port', app.get('port'));
 });
+
+
+//server will need to get users personality data upon authentication
