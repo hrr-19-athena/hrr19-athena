594e11bb99bedffa23190178a75a3aaac5bdf919
Vi Vo <vi.uyen.vo@gmail.com>
2016-10-24 11:08:31 -0700

    watson successfully returns analysis with hard-coded data - vi

--- server/server.js
+++ server/server.js
@@ -1,12 +1,62 @@
 var express = require('express');
+var app        = express();
+var bluemix    = require('../node_modules/bluemix/lib/bluemix.js');
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+var extend     = require('util')._extend;
+var keys = require('./api-services.js');
 
-var app = express();
+//CREDENTIALS IF WE NEED TO GENERATE ON THE FLY - still buggy - Vi
+// var credentials = extend({
+//   version:'v2',
+//   username: username,
+//   password: password
+// }), bluemix.getServiceCreds('personality_insights'));
 
-app.set('port', 3000);
+// // Create the service wrapper
+// var personality_insights = watson.personality_insights(credentials);
 
-app.listen(app.get('port'), function() {
-  console.log('Server listening on port', app.get('port'));
+//CREDENTIALS SECTION - Vi
+var personality_insights = new PersonalityInsightsV3({
+  username: keys.watsonPersonality.username,
+  password: keys.watsonPersonality.password,
+  version_date: '2016-10-20'
 });
 
+//TEMPORARY WATSON FOR FAKE HARD-CODED DATA - Vi
+var params = {
+  content_items: require('./profile.json').contentItems,
+  consumption_preferences: true,
+  raw_scores: true,
+  headers: {
+    'accept-language': 'en',
+    'accept': 'application/json'
+  }
+};
 
-//server will need to get users personality data upon authentication
+//TEMPORARY WATSON FOR FAKE HARDCODED DATA -Vi
+personality_insights.profile(params, function(error, response) {
+  if(error) {
+    console.log('error: ', error);
+  } else {
+    console.log(JSON.stringify(response, null, 2));
+  }
+});
+
+//FOR WATSON LATER ON WITH REAL DATA -Vi
+// app.post('/', function(req, res, next) {
+//   personality_insights.profile(req.body, function(err, profile) {
+//     if (err)
+//       return next(err);
+//     else
+//       return res.json(profile);
+//   });
+// });
+
+// render index page
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
