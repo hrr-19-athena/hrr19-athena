3c72b7c492c4d4a4b059e27afa2917d4f0a84c7a
Vi Vo <vi.uyen.vo@gmail.com>
2016-10-24 20:54:31 -0700

    oauth + twitter feed - nam vi

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ var express = require('express');
 //var morgan = require('morgan');
 var bodyParser = require('body-parser');
 // var jwt = require('express-jwt');
-var bluemix = require('../node_modules/bluemix/lib/bluemix.js');
+//bluemix is for if we want to generate keys dynamically, we are not currently - vi
+// var bluemix = require('../node_modules/bluemix/lib/bluemix.js');
 var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 var extend = require('util')._extend;
 var keys = require('./api-services.js');
+var Twitter = require('twitter-node-client').Twitter;
 
 
 //EXPRESS SERVER
@@ -20,6 +22,59 @@ app.use(express.static('__dirname/../client'));
 app.listen(app.get('port'), function() {
   console.log('Listening on port', app.get('port'));
 });
+
+//WHATS GOING ON:
+//REQ from front end
+//app.post(/micahlesPersonalRoute, function(req, res){
+  //assume req.body.id, req.body.token
+  //use id to do a database lookup
+    //2 scenarios:
+      //1: USER NOT IN DATABSE
+          //MICHAEL intiate get analysis(pass through twitter, pass through watson, this is lots of work we need to divy this)
+              //parts of get analysis
+                  //1. get twitter feed using userId, massage data, (later save to db $$$$$$)
+                  //2. feed the twitter data into watson, save watson data to db (just the 5 personalities portion)
+                  //3. send data to front end
+              //how to group people -- later, group undefined
+      //2: USER IS IN DATABASE
+          //MICHAEL pull watson user results from database, send to front end
+
+
+//TWITTER
+
+var userTweetsArray = [];
+var userTweets = {
+  content: "",
+  created: "",
+  id: "",
+  language: ""
+};
+
+var error = function (err, response, body) {
+  console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+  console.log('Data [%s]', JSON.parse(data)[0].id);
+  console.log('Data [%s]', JSON.parse(data)[0].text);
+  console.log('Data [%s]', JSON.parse(data)[0].lang);
+  console.log('Data [%s]', JSON.parse(data)[0].created_at);
+};
+
+// var config = {
+//   'consumerKey': keys.twitter.key,
+//   'consumerSecret': keys.twitter.secret,
+//   'accessToken': keys.twitter.token,
+//   'accessTokenSecret': keys.twitter.tokenSecret,
+//   'callBackUrl': 'https://hrr19-athena.herokuapp.com/api/twitter'
+// };
+
+//CALLBACK URL ROUTE
+// app.get('/api/twitter', function());
+
+var Twitter = require('twitter-node-client').Twitter;
+var twitter = new Twitter();
+
+twitter.getUserTimeline({ screen_name: 'HackReactor', count: '20'}, error, success);
 
 //AUTH0 api call
 //app.get('/api/clientcred', )
@@ -53,13 +108,13 @@ var params = {
 };
 
 //TEMPORARY WATSON FOR FAKE HARDCODED DATA -Vi
-personality_insights.profile(params, function(error, response) {
-  if(error) {
-    console.log('error: ', error);
-  } else {
-    console.log(JSON.stringify(response, null, 2));
-  }
-});
+// personality_insights.profile(params, function(error, response) {
+//   if(error) {
+//     console.log('error: ', error);
+//   } else {
+//     console.log(JSON.stringify(response, null, 2));
+//   }
+// });
 
 //FOR WATSON LATER ON WITH REAL DATA -Vi
 // app.post('/', function(req, res, next) {
