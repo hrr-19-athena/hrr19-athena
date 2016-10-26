4feef65cdcfcbe9179093d229979b2f30841cae6
Michael Oliver <jmichaeloliver12@gmail.com>
2016-10-25 20:26:44 -0600

    updated server

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ db.on('open', function(){
   console.log('opened');
 });
 
+//------------- EXPRESS SERVER ---------------------
 
-//EXPRESS SERVER
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -36,7 +36,7 @@ app.listen(app.get('port'), function() {
 });
 
 
-//AUTH0 api call
+//------------ AUTH0 api call -----------------------
 app.get('/api/clientcred', function(req, res) {
   var Auth0Cred = {
     AUTH0_CLIENT_ID : process.env.AUTH0_CLIENT_ID,
@@ -45,27 +45,20 @@ app.get('/api/clientcred', function(req, res) {
   res.send(Auth0Cred);
 });
 
-//DATABASE ROUTES
 
+//-------------- DATABASE ROUTES -------------
 app.post('/api/users/signin', function(req, res) {
   //list one user
 });
 app.post('/api/users/signup', userController.addUser);
 
+app.get('/api/findAllUsers', userController.listAllUsers);
+app.get('/api/findOneUser', userController.find);
 
-// app.get('/api/findAllUsers', userController.listAllUsers);
-// app.get('/api/findOneUser', userController.find);
+//personaController - where the magic happens
+app.get('/profileAnalysis', Persona.personaData);
+  //when profile page loads for user gets finished results for current
 
-// //personaController - where the magic happens
-// app.get('/profileAnalysis', function(req, res) {
-//   //when profile page loads for user gets finished results for current
-// })
-
-// app.post('/genAnalysis', function(req, res) {
-//   //user data sent in
-//   //check in db if persona data exists
-//   //if not generate
-// })
 
 
 //WHATS GOING ON:
@@ -85,50 +78,13 @@ app.post('/api/users/signup', userController.addUser);
           //MICHAEL pull watson user results from database, send to front end
 
 
-//TWITTER
 
-var userTweetsArray = [];
-var userTweets = {
-  content: "",
-  created: "",
-  id: "",
-  language: ""
-};
-
-// var error = function (err, response, body) {
-//   console.log('ERROR [%s]', err);
-// };
-// var success = function (data) {
-//   console.log('Data [%s]', JSON.parse(data)[0].id);
-//   console.log('Data [%s]', JSON.parse(data)[0].text);
-//   console.log('Data [%s]', JSON.parse(data)[0].lang);
-//   console.log('Data [%s]', JSON.parse(data)[0].created_at);
-// };
-
-// var config = {
-//   'consumerKey': keys.twitter.key,
-//   'consumerSecret': keys.twitter.secret,
-//   'accessToken': keys.twitter.token,
-//   'accessTokenSecret': keys.twitter.tokenSecret,
-//   'callBackUrl': 'https://hrr19-athena.herokuapp.com/api/twitter'
-// };
-
-//CALLBACK URL ROUTE
-// app.get('/api/twitter', function());
-
-// var Twitter = require('twitter-node-client').Twitter;
-// var twitter = new Twitter();
-
-// twitter.getUserTimeline({ screen_name: 'HackReactor', count: '20'}, error, success);
-
-
-
-
-
-
-
-//HOW TO USE HANDLEWATSONPERSONA -Vi
-// app.get('/personality', function(req, res) {
-//     watson.handleWatsonPersona(watson.params, '9', res);
+//TEMPORARY WATSON FOR FAKE HARDCODED DATA -Vi
+// watson.personality_insights.profile(watson.params, function(error, response) {
+//   if(error) {
+//     console.log('error: ', error);
+//   } else {
+//     console.log(JSON.stringify(response, null, 2));
+//   }
 // });
 
