4dfca308847be5b00c78d9b7b24771a179e673f8
Vi Vo <vi.uyen.vo@gmail.com>
2016-10-29 11:23:39 -0700

    code cleanup for watsonController

--- server/watson/watsonController.js
+++ server/watson/watsonController.js
@@ -2,7 +2,6 @@ var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights
 var keys = require('../api-services.js');
 var UserModel = require('../user/userModel.js');
 var Q = require('q');
-// Promisify mongoose methods with the `q` promise library
 var findUser = Q.nbind(UserModel.findOne, UserModel);
 var createUser = Q.nbind(UserModel.create, UserModel);
 var findAll = Q.nbind(UserModel.find, UserModel);
@@ -11,20 +10,6 @@ var userController = require('../user/userController.js');
 var _ = require('underscore');
 require('dotenv').config();
 
-//var objectToJson = require("object-to-json");
-// var jsonPerson = objectToJSON(person);
-//bluemix is for if we want to generate keys dynamically, we are not currently - vi
-// var bluemix = require('../node_modules/bluemix/lib/bluemix.js');
-
-//CREDENTIALS IF WE NEED TO GENERATE ON THE FLY - still buggy - Vi
-// var credentials = extend({
-//   version:'v2',
-//   username: username,
-//   password: password
-// }), bluemix.getServiceCreds('personality_insights'));
-
-// // Create the service wrapper
-// var personality_insights = watson.personality_insights(credentials);
 
 //CREDENTIALS SECTION - Vi
 module.exports.personality_insights = new PersonalityInsightsV3({
@@ -33,25 +18,12 @@ module.exports.personality_insights = new PersonalityInsightsV3({
   version_date: '2016-10-20'
 });
 
-//TEMPORARY WATSON FOR FAKE HARD-CODED DATA - Vi
-// module.exports.params = {
-//   content_items: require('../profile.json').contentItems,
-//   consumption_preferences: true,
-//   raw_scores: true,
-//   headers: {
-//     'accept-language': 'en',
-//     'accept': 'application/json'
-//   }
-// };
 
 module.exports.handleWatsonPersona = function(twitterFeed, userId, res){
   id = userId || 'HackReactor';
   var regex = /[a-zA-Z0-9^/:" "{},]/g;
   var results = twitterFeed.match(regex);
   var contentArray = [results.join('')];
-  // console.log('Twitter Content Loading into Watson')
-  //console.log('ContentArray: ', contentArray);
-  // console.log(results.join(''));
   var params = {
     content_items: contentArray,
     consumption_preferences: true,
@@ -66,52 +38,35 @@ module.exports.handleWatsonPersona = function(twitterFeed, userId, res){
     if (err)
       console.log(err);
     else
-      // res.json(profile.personality);
       module.exports.massageAndSave(profile, id, res);
   });
 };
 
 module.exports.findSimilar = function(profile, id) {
-  // console.log('got into findSimialr!');
-  //var group
-  // console.log('profile is ', profile);
-  // console.log('profile.persona is ', profile.persona);
-  // console.log('profile.persona[0].percentile is ', profile.persona[0].percentile);
   var curUserId = profile.userId || id;
   var profile = profile.persona || profile.personality;
   var similarGroup = [];
-  //create trait summary for Cur
   var curTS = [];
   for (var i = 0; i< profile.length; i++) {
     curTS.push(0.5 - profile[i].percentile);
   }
-  // console.log('This person\'s trait summary is ', curTS);
-  //find all people in database,
  return Q(UserModel.find({}).exec())
     .then(function(users) {
-    //create trait summary for each user in database
       for (var j = 0; j<users.length; j++) {
         var userTS = [];
         var curUser = users[j].persona;
-        // console.log('each user is ', curUser);
         for(var k = 0; k<curUser.length; k++) {
           userTS.push(0.5 - curUser[k].percentile);
         }
-        // console.log('individual userTS is ', userTS);
     //calculate gap for each
-      //if gap < 5, add person to group
         var gap = 0;
         for(var l = 0; l<userTS.length; l++) {
           gap += (curTS[l] - userTS[l]);
         }
-        // console.log('gap is ', gap);
         if(Math.abs(gap) < 0.5 && users[j].userId !== curUserId){
           similarGroup.push(users[j]);
-          // console.log(similarGroup);
         }
       }
-  //return group
-    // console.log('similarGroup from watsonController ', similarGroup);
     return similarGroup;
     });
 };
@@ -142,8 +97,6 @@ module.exports.massageAndSave = function(profile, query, res){
         img:query.img,
         description: query.description
       };
-      // console.log('group from data is ', group);
-      // req.body.user='HackReactor';
       userController.addUser(data);
 
       var sendBack = {
@@ -157,21 +110,25 @@ module.exports.massageAndSave = function(profile, query, res){
     });
 };
 
-//FOR WATSON LATER ON WITH REAL DATA -Vi
-// app.get('/personality', function(req, res, next) {
-//   personality_insights.profile(req.body, function(err, profile) {
-//     if (err)
-//       return next(err);
-//     else
-//       watson.handleWatsonPersona(profile);
-//   });
+//CREDENTIALS IF WE NEED TO GENERATE ON THE FLY
+//bluemix is for if we want to generate keys dynamically, we are not currently
+// var bluemix = require('../node_modules/bluemix/lib/bluemix.js');
+// var credentials = extend({
+//   version:'v2',
+//   username: username,
+//   password: password
+// }), bluemix.getServiceCreds('personality_insights'));
 
-  //FOR WATSON LATER ON WITH REAL DATA -Vi
-// app.get('/personality', function(req, res, next) {
-//   personality_insights.profile(req.body, function(err, profile) {
-//     if (err)
-//       return next(err);
-//     else
-//       return res.json(profile);
-//   });
-// });
+// // Create the service wrapper
+// var personality_insights = watson.personality_insights(credentials);
+
+//TEMPORARY WATSON FOR FAKE HARD-CODED DATA
+// module.exports.params = {
+//   content_items: require('../profile.json').contentItems,
+//   consumption_preferences: true,
+//   raw_scores: true,
+//   headers: {
+//     'accept-language': 'en',
+//     'accept': 'application/json'
+//   }
+// };
